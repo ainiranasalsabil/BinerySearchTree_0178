581b8799e211fed0ba8bb5558cadbfebbc7f5f90
ainiranasalsabil <ainiranasalsabil04@gmail.com>
2024-06-13 15:26:51 +0700

    Tambahkan fungsi search ke BinaryTree

--- BinerySearchTree/BinerySearchTree.cpp
+++ BinerySearchTree/BinerySearchTree.cpp
@@ -55,4 +55,19 @@ public:
 		}
 	}
 
+	void search(string element, Node*& parent, Node*& currentNode)
+	{
+		//this function searches the currentNode of the specified Node as well as the current Node of its parent
+		currentNode = ROOT;
+		parent = nullptr;
+		while ((currentNode != nullptr) && (currentNode->info != element))
+		{
+			parent = currentNode;
+			if (element < currentNode->info)
+				currentNode = currentNode->leftchild;
+			else
+				currentNode = currentNode->rightchild;
+		}
+	}
+
 	

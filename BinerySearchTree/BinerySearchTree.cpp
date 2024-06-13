63ee38eb4db88c6446bd4e6363c5d85d54131032
ainiranasalsabil <ainiranasalsabil04@gmail.com>
2024-06-13 15:26:12 +0700

    Implementasi fungsi insert dalam BinaryTree

--- BinerySearchTree/BinerySearchTree.cpp
+++ BinerySearchTree/BinerySearchTree.cpp
@@ -28,4 +28,31 @@ public:
 		ROOT = nullptr; //Initializing ROOT to null
 	}
 
+	void insert(string element) //insert a node the binary search tree
+	{
+		Node* newNode = new Node(element, nullptr, nullptr); //Allocation memory for new node
+		newNode->info = element; //Assign value to the data field for ner node
+		newNode->leftchild = nullptr; //Make the left child of the new node point to NULL
+		newNode->rightchild = nullptr; //Make the right child of the new node point to NULL
+
+		Node* parent = nullptr;
+		Node* currentNode = nullptr;
+		search(element, parent, currentNode);// Locate the node which will br thr parent of the node to be 
+
+		if (parent == nullptr) //if the parent is NULL (Tree is empty)
+		{
+			ROOT = newNode; // mark the new node as ROOT
+			return; // exit
+		}
+
+		if (element < parent->info) // if the value in the data field of the new Node is the less than that of
+		{
+			parent->leftchild = newNode; // make the left child of the parent point to the new node 
+		}
+		else if (element > parent->info) // if the value in the data field of the new node is greater than that parent
+		{
+			parent->rightchild = newNode; // make the right child of the parent point to the new node
+		}
+	}
+
 	
